596682f8c363bfe16b696088c0342297f2bf67b9
research-patron <77576064+research-patron@users.noreply.github.com>
2025-03-09 12:39:59 +0900

    Update firebase.ts

--- frontend/src/api/firebase.ts
+++ frontend/src/api/firebase.ts
@@ -7,14 +7,7 @@ import { getStorage } from 'firebase/storage';
 // Firebaseの設定
 // 注: 実際のプロジェクトでは環境変数から読み込むことを推奨
 const firebaseConfig = {
-    apiKey: "AIzaSyD2UPHxj9TPM4fkxKFtXq05T45DASzgf48",
-    authDomain: "smart-paper-v2.firebaseapp.com",
-    databaseURL: "https://smart-paper-v2-default-rtdb.firebaseio.com",
-    projectId: "smart-paper-v2",
-    storageBucket: "smart-paper-v2.firebasestorage.app",
-    messagingSenderId: "137197753964",
-    appId: "1:137197753964:web:d4909af8346cf602e8aa49",
-    measurementId: "G-RQ1V5EN6CN"
+    
   };
 
 // Firebaseを初期化
